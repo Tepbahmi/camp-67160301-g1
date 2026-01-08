a610c6e6d46efef57d144ce65ce31a24721a797e
Tepbahmi <67160301@go.buu.ac.th>
2026-01-08 16:49:36 +0700

    ทำ CRUD#pokedexs

--- resources/views/pokedexs/show.blade.php
+++ resources/views/pokedexs/show.blade.php
@@ -1,19 +1,33 @@
 @extends('template.default')
+
 @section('content')
-<h1>{{ $pokedex->name }}</h1>
-@if($pokedex->image_url)
-    <img src="{{ $pokedex->image_url ?? 'https://via.placeholder.com/200' }}" width="200">
-@endif
+<div class="container mt-4">
 
-<ul>
-    <li>Type : {{ $pokedex->type }}</li>
-    <li>Species : {{ $pokedex->species }}</li>
-    <li>Height : {{ $pokedex->height }} M</li>
-    <li>Weight : {{ $pokedex->weight }} Kg</li>
-    <li>HP : {{ $pokedex->hp }}</li>
-    <li>Attack : {{ $pokedex->attack }}</li>
-    <li>Defense : {{ $pokedex->defense }}</li>
-</ul>
+    <div class="border rounded p-4 shadow-sm mx-auto" style="max-width: 450px; width: 100%;">
+        <h2 class="mb-3 text-center">{{ $pokedex->name }}</h1>
 
-<a class="btn btn-secondary" href="{{ url('/pokedexs') }}">ย้อนกลับ</a>
+        @if($pokedex->image_url)
+            <div class="text-center mb-3">
+                <img src="{{ $pokedex->image_url ?? 'https://via.placeholder.com/200' }}"
+                     width="200"
+                     class="img-fluid rounded">
+            </div>
+        @endif
+
+        <ul class="list-unstyled">
+            <li><strong>Type :</strong> {{ $pokedex->type }}</li>
+            <li><strong>Species :</strong> {{ $pokedex->species }}</li>
+            <li><strong>Height :</strong> {{ $pokedex->height }} M</li>
+            <li><strong>Weight :</strong> {{ $pokedex->weight }} Kg</li>
+            <li><strong>HP :</strong> {{ $pokedex->hp }}</li>
+            <li><strong>Attack :</strong> {{ $pokedex->attack }}</li>
+            <li><strong>Defense :</strong> {{ $pokedex->defense }}</li>
+        </ul>
+
+        <div class="text-center mt-3">
+            <a class="btn btn-secondary" href="{{ url('/pokedexs') }}">ย้อนกลับ</a>
+        </div>
+    </div>
+
+</div>
 @endsection
